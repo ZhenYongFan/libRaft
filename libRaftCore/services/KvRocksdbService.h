04b84517421a05aa28a4b6c13cd2599686945f52
ZhenYongFan <fanzhenyong@gbase.cn>
2019-07-24 00:05:28 +0800

    kv service的简单实现

---
+++ libRaftCore/services/KvRocksdbService.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "KvService.h"
+
+namespace rocksdb
+{
+    class DB;
+};
+
+///\brief 使用Rocksdb实现KV服务
+class LIBRAFTCORE_API CKvRocksdbService :public CKvService
+{
+public:
+    ///\brief 构造函数
+    CKvRocksdbService(void);
+
+    ///\brief 析构函数
+    virtual ~CKvRocksdbService(void);
+    
+    ///\brief 初始化
+    ///\param strDbPath 存放数据的目录
+    ///\return 成功标志 0 成功；其他 失败
+    virtual int Init(std::string &strDbPath);
+
+    ///\brief 释放资源
+    virtual void Uninit(void);
+
+    ///\brief Put操作
+    ///\param strKey Key
+    ///\param strValue Value
+    ///\return 成功标志 0 成功；其他 失败
+    virtual int Put(const std::string &strKey, const std::string &strValue);
+
+    ///\brief Put操作
+    ///\param strKey Key
+    ///\param strValue 如果成功返回的Value
+    ///\return 成功标志 0 成功；其他 失败
+    virtual int Get(const std::string &strKey, std::string &strValue);
+
+    ///\brief Delete操作
+    ///\param strKey Key
+    ///\return 成功标志 0 成功；其他 失败
+    virtual int Delete(const std::string &strKey);
+protected:
+    rocksdb::DB * m_pStocksDB; ///< RocksDB对象
+};
